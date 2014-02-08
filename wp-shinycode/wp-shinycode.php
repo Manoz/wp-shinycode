2d59a7825e987b4f1034723dc864d497ba72da65
Manoz <manoz@outlook.com>
2014-02-08 19:25:12 +0100

    1st commit

---
+++ wp-shinycode/wp-shinycode.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Plugin Name:     WP Shiny Code
+ * Plugin URI:      https://github.com/Manoz/wp-shinycode
+ * Description:
+ * Version:         1.0.0
+ * Author:          Kevin Legrand
+ * Author URI:      http://www.k-legrand.fr/
+ * Text Domain:     shinycode
+ * License:         GPL-2.0+
+ * License URI:     http://www.gnu.org/licenses/gpl-2.0.txt
+ * Domain Path:     /languages
+ *
+ * @package   WP Clean
+ * @author    Kevin Legrand <manoz@outlook.com>
+ * @license   GPL-2.0+
+ * @link      http://www.k-legrand.fr/
+ * @copyright 2014 Kevin Legrand
+ *
+ * Icons finded on https://www.iconfinder.com/
+ */
+if ( !defined( 'ABSPATH' )) {
+    exit();
+}
