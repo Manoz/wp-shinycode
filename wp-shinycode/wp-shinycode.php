1e27d63f3c3f7c5b8f12ab64cc53bc28876dfbaf
Manoz <manoz@outlook.com>
2014-02-08 19:26:04 +0100

    update main plugin file

--- wp-shinycode/wp-shinycode.php
+++ wp-shinycode/wp-shinycode.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name:     WP Shiny Code
  * Plugin URI:      https://github.com/Manoz/wp-shinycode
- * Description:
+ * Description:     WP Shiny code is another syntax highlighting plugin for WordPress. It uses GeSHi library and of course, is server-side.
  * Version:         1.0.0
  * Author:          Kevin Legrand
  * Author URI:      http://www.k-legrand.fr/
@@ -11,13 +11,12 @@
  * License URI:     http://www.gnu.org/licenses/gpl-2.0.txt
  * Domain Path:     /languages
  *
- * @package   WP Clean
+ * @package   WP Shiny Code
  * @author    Kevin Legrand <manoz@outlook.com>
  * @license   GPL-2.0+
  * @link      http://www.k-legrand.fr/
  * @copyright 2014 Kevin Legrand
  *
- * Icons finded on https://www.iconfinder.com/
  */
 if ( !defined( 'ABSPATH' )) {
     exit();
