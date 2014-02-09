9501bd836f2b0994d84653aa42f292ad664b37bc
Manoz <manoz@outlook.com>
2014-02-09 02:51:56 +0100

    Add lot of stuff with no commit details...

---
+++ wp-shinycode/includes/js/tinymce.js
@@ -0,0 +1,55 @@
+jQuery(document).ready(function($) {
+    tinymce.create('tinymce.plugins.sc', {
+        init : function(ed, url) {
+            // Add code
+            $(document).on( 'click','#shinycode-insert', function( e ) {
+                e.preventDefault();
+                ed.execCommand(
+                    'mceInsertContent',
+                    false,
+                    shinycode_create_shortcode()
+                );
+                tb_remove();
+            });
+            ed.addButton('sc', {
+                title : 'Shiny Code',
+                image : url+'/../images/shinycode-button.png',
+                onclick : function() {
+                    tb_show('Shinycode', ajaxurl+'?action=shinycode_ajax_shortcodes&width=600&height=410');
+                    $("#TB_ajaxContent").css('overflow',"visible");
+                    setTimeout(function() {
+                        $('#TB_window').css({'height':'450px', 'marginTop':($(window).height()-450) / 2});
+                    },800);
+                }
+            });
+        },
+    });
+    tinymce.PluginManager.add('sc', tinymce.plugins.sc);
+});
+
+// Add some magic to replace special chars
+function shinycode_esc_html( str ) {
+    return String(str).replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&#34;').replace(/'/g, '&#039;');
+}
+
+function shinycode_create() {
+    var inputs = jQuery('#shinycode-ajax-form').serializeArray();
+    var shortcode = '[shinycode ';
+    var textarea = '';
+    for ( var a in inputs ) {
+        if ( inputs[a].value == "" ||  inputs[a].value == undefined)
+            continue;
+        if ( inputs[a].name=='shinycode-manual' )
+            textarea = inputs[a].value;
+        else {
+            inputs[a].name = inputs[a].name.replace( 'shinycode-', '' );
+            shortcode += ' '+inputs[a].name+'="'+inputs[a].value+'"';
+        }
+    }
+
+    if ( textarea!='')
+        shortcode += ']<pre><code>' + shinycode_esc_html( textarea ) + '</code></pre>[/shinycode]';
+    else
+        shortcode += '/]';
+    return shortcode;
+}
--- wp-shinycode/wp-shinycode.php
+++ wp-shinycode/wp-shinycode.php
@@ -27,8 +27,8 @@ define( 'SHINYCODE_VERSION', '1.0.0' );
  */
 add_action( 'plugins_loaded', 'shinycode_languages' );
 function shinycode_languages() {
-    $plugin_dir = basename(dirname(__FILE__));
-    load_plugin_textdomain( 'shinycode', false, "$plugin_dir/languages");
+    $plugin_dir = basename( dirname( __FILE__ ) );
+    load_plugin_textdomain( 'shinycode', false, "$plugin_dir/languages" );
 }
 
 /**
@@ -45,6 +45,7 @@ function shinycode_action_links( $links ) {
 
 /**
  * Add some useful links in plugins page (row meta)
+ * @todo add URL to FAQ and Support.
  * @since 1.0.0
  */
 add_filter( 'plugin_row_meta', 'shinycode_row_meta', 10, 2 );
@@ -74,7 +75,7 @@ function shinycode_admin_menu() {
     register_setting( 'shinycode', 'shinycode_theme' );
     add_settings_section(
         'shinycode_settings_section',               // Section ID (used to add fields)
-        __( 'Shinycode Settings', 'shinycode' ),    // Section Title
+        __( 'Shiny Code settings', 'shinycode' ),    // Section Title
         'shinycode_section_callback',               // Callback function (description)
         'shinycode'                                 // Page ID (same as menu slug)
     );
@@ -132,6 +133,7 @@ function shinycode_field_callback( $args ) {
     foreach( $options as $key => $option )
         echo '<option value="' . $key . '" ' . selected( $old==$key, true, false ) . '>' . esc_html( $option ) . '</option>';
     echo '</select>';
+    echo '<label for="' . $name . '"> '. __('Choose your favorite syntax coloration theme.', 'shinycode') .'</label>';
 }
 
 /**
@@ -147,3 +149,82 @@ register_uninstall_hook( __FILE__, 'shinycode_uninstall' );
 function shinycode_uninstall() {
     delete_option( 'shinycode_theme' );
 }
+
+/**
+ * Add TinyMCE shortcodes button
+ * @since 1.0.0
+ */
+add_action( 'admin_init', 'shinycode_tinymce_button' );
+function shinycode_tinymce_button() {
+    if ( ! current_user_can('edit_posts') && ! current_user_can('edit_pages') )
+        return false;
+    if ( get_user_option('rich_editing') == 'true') {
+        add_filter( 'mce_external_plugins', 'add_shinycode_mce' );
+        add_filter( 'mce_buttons', 'shinycode_register_button' );
+    }
+}
+
+function add_shinycode_mce( $plugin_array ) {
+    $plugin_array['sc'] = plugin_dir_url( __FILE__ ) .'/includes/js/tinymce.js';
+    return $plugin_array;
+}
+
+function shinycode_register_button( $buttons ) {
+    array_push($buttons, "|", "sc");
+    return $buttons;
+}
+
+add_action( 'wp_ajax_shinycode_ajax_shortcodes', 'wp_ajax_shinycode_box' );
+function wp_ajax_shinycode_box() {
+    global $wp_styles;
+    if ( !empty($wp_styles->concat) ) {
+        $dir = $wp_styles->text_direction;
+        $ver = md5("$wp_styles->concat_version{$dir}");
+
+        $href = $wp_styles->base_url . "/wp-admin/load-styles.php?c={$zip}&dir={$dir}&load=media&ver=$ver";
+        echo "<link rel='stylesheet' href='" . esc_attr( $href ) . "' type='text/css' media='all' />\n";
+    } ?>
+
+    <h3 class="media-title"><?php _e('Shiny Code', 'shinycode'); ?></h3>
+    <form name="shinycode-ajax-form" id="shinycode-ajax-form">
+        <div id="media-items">
+            <div class="media-item media-blank">
+                <table class="describe" style="width:100%;margin-top:1em;">
+                    <tbody>
+                        <tr valign="top" class="field">
+                            <th class="label" scope="row">
+                                <label for="shinycode-language"><?php _e('Select a language', 'shinycode'); ?></label>
+                                <td>
+                                    <select name="shinycode-language" id="shinycode-language">
+                                        <optgroup label="<?php _e( 'Select a language', 'shinycode' ); ?>">
+                                        <?php
+                                        $langs  = array(
+                                            'markup'        => 'HTML',
+                                            'css'           => 'CSS',
+                                            'javascript'    => 'JavaScript',
+                                            'jquery'        => 'jQuery',
+                                            'php'           => 'PHP',
+                                            'htmlphp'       => 'HTML + PHP',
+                                        );
+                                        $langs = apply_filters( 'shinycode_langs', $langs );
+                                        foreach( $langs as $l => $lang )
+                                            echo '<option value="' . $l . '">' . $lang . '</value>';
+                                        unset( $l );
+                                        ?>
+                                        </optgroup>
+                                    </select>
+                                </td>
+                            </th>
+                        </tr>
+                        <tr valign="top" class="field">
+                            <td>
+                                <p class="current-page"><input name="shinycode-insert" type="submit" class="button-primary" id="shinycode-insert" tabindex="5" accesskey="p" value="<?php _e('Insert this Shiny Code', 'shinycode') ?>"></p>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </form>
+
+<?php }
